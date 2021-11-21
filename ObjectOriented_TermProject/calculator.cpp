0eee157d6e25087756d2da1ae94451d33170235c
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2021-11-21 15:00:13 +0900

    종료처리 수정

--- ObjectOriented_TermProject/calculator.cpp
+++ ObjectOriented_TermProject/calculator.cpp
@@ -8,22 +8,37 @@ using namespace std;
 int main()
 {
 	string input;
+	int quit = 0;
+	cout << "종료를 원하면 " << "q" << " 입력" << endl;
 	getline(cin, input);
-	while (input.empty()) {
-		cout << "아무것도 입력하지 않았습니다. 식을 입력해 주세요." << endl;
-		getline(cin, input);
-	}
-	string postfix = Convert(input);
-	while (postfix.compare("error") == 0) {
-		cout << "잘못된 입력입니다. 다시 한번 입력해 주세요." << endl;
-		getline(cin, input);
+	while (1) {
 		while (input.empty()) {
 			cout << "아무것도 입력하지 않았습니다. 식을 입력해 주세요." << endl;
 			getline(cin, input);
 		}
-		postfix = Convert(input);
+		if (input.compare("q") == 0) {
+			break;
+		}
+		string postfix = Convert(input);
+		while (postfix.compare("error") == 0) {
+			cout << "잘못된 입력입니다. 다시 한번 입력해 주세요." << endl;
+			getline(cin, input);
+			while (input.empty()) {
+				cout << "아무것도 입력하지 않았습니다. 식을 입력해 주세요." << endl;
+				getline(cin, input);
+			}
+			if (input.compare("q") == 0) {
+				quit = 1;
+				break;
+			}
+			postfix = Convert(input);
+		}
+		//cout << postfix << endl;
+		if (quit)
+			break;
+		cout << calculate(postfix) << endl;
+		getline(cin, input);
 	}
-	//cout << postfix << endl;
-	cout << calculate(postfix) << endl;
+
 	return 0;
 }
